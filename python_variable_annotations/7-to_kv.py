8646ce85b21bf52e07299dc5f4bff0098f223b43
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-08-24 10:28:12 +0000

    Add: task 7: Complex types - string and int/float to tuple

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""this is a module for the 7th task"""
+from typing import List, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """this is the method"""
+    return (k, float(v ** 2))
