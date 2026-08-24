82828ec5150b7c80a1db21578baa1b439c411d5f
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-08-24 10:42:39 +0000

    Add

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""this is a module for the last task"""
+import typing
+
+
+def element_length(
+    lst: typing.Iterable[typing.Sequence]
+) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+    """Calculates the length of each sequence element within a generic iterable."""
+    return [(i, len(i)) for i in lst]
