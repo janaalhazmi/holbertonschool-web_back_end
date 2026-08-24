3b9cdc63e76da09ecaaedaa392a779f423e74fe2
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-08-24 10:25:25 +0000

    Add: task 6:Complex types - mixed list

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -3,6 +3,6 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst:List[Union[int, float]]):
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """this is the method"""
     return float(sum(mxd_lst))
