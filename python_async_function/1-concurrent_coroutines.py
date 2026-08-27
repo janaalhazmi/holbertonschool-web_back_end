956078fbb589e1a732ce08830ace8ed12b06cdfd
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-08-27 10:06:58 +0000

    Add: task 1: The basics of async

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""this is the module for the second task"""
+
+
+import asyncio
+from typing import List
+import random
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """this is the method"""
+    l = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
+    finish = [await task for task in asyncio.as_completed(l)]
+    return finish
