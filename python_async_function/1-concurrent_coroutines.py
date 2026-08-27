2dc6789f67864db472e1199e3ce246bd168dee56
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-08-27 10:18:36 +0000

    Add: task 1: Lets execute..

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -10,5 +10,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """this is the method"""
     delays = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
-    finish = [await task for task in asyncio.as_completed(l)]
+    finish = [await task for task in asyncio.as_completed(delays)]
     return finish
