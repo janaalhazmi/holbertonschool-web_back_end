ade2f614d7a7326470649b386ec1c02e4afa519c
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-09-12 12:53:04 +0000

    Add : task 10 :Change school topics

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -4,7 +4,7 @@
 
 def update_topics(mongo_collection, name, topics):
     """this is the method"""
-   mongo_collection.update_many(
+    mongo_collection.update_many(
         {"name": name},
         {"$set": {"topics": topics}}
     )
