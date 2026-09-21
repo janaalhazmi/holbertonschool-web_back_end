49db8179a116b6ded549d5d0d0022307ee4c1953
Jana Alhazmi <janaalhazmi78@gmail.com>
2026-09-21 08:43:30 +0000

    Add : task 8 : Clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,13 @@
 export default function cleanSet(set, startString) {
-  let result = '';
-
-  for (const value of set) {
-    if (value.startsWith(startString)) {
-      result += value.slice(startString.length) + '-';
+    if (!(set instanceof Set) || typeof startString !== 'string' || startString.length === 0) {
+        return '';
     }
-  }
-
-  return result;
+    let result = '';
+    for (const value of set) {
+        if (value.startsWith(startString)) {
+            result += value.substr(startString.length);
+            result += '-';
+        }
+    }
+    return result.slice(0, -1);
 }
