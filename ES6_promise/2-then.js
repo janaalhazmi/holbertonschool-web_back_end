ccf07de6491bc8f484501646142b5ae7e787cb03
janaalhazmi <janaalhazmi78@gmail.com>
2026-09-25 06:31:33 +0300

    Add: task 2: Catch me if you can!

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 export default function handleResponseFromAPI(promise) {
     return promise 
-        .then(() => { return { status: 200, body: 'Success' }; })
+        .then(() => { return { status: 200, body: Success }; })
         .catch(() => { return new Error() })
         .finally(() => { console.log('Got a response from the API'); });
 }   
